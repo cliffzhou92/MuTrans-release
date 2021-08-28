964b4550eecbf6d8224cf60344debbb183fcc838
cliffzhou92 <46710747+cliffzhou92@users.noreply.github.com>
2021-08-27 18:45:10 -0700

    update codes

--- MuTrans/Visualization/DM_forced2d.m
+++
@@ -1,7 +0,0 @@
-function mappedX = DM_forced2d (data,par)
-dims = par.dims;
-sigma = par.sigma;
-k = par.isomap_kn;
-[phi,~] = diffusionmap(data,'loc',sigma,dims);
-mappedX = isomap(phi, 2, k);
-end
--- MuTrans/Visualization/SC_forced2d.m
+++
@@ -1,6 +0,0 @@
-function mappedX = SC_forced2d (P,par)
-dims = par.dims;
-k = par.isomap_kn;
-[phi,~] = eigs(P,dims+1);
-mappedX = isomap(phi(:,2:end), 2, k);
-end
